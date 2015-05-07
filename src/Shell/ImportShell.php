12328359525f834aa8cecd3504cbafcb1ba85a11
karolck <karolck@gmail.com>
2015-05-07 21:53:22 +0200

    import country name to our database

--- src/Shell/ImportShell.php
+++ src/Shell/ImportShell.php
@@ -3,6 +3,8 @@
 namespace App\Shell;
 
 use Cake\Console\Shell;
+use Cake\ORM\TableRegistry;
+
 
 class ImportShell extends Shell {
 
@@ -11,9 +13,25 @@ class ImportShell extends Shell {
         $countriesFile = file_get_contents('https://raw.githubusercontent.com/mledoze/countries/master/dist/countries.json');
 
         $countries = json_decode(utf8_encode($countriesFile));
+        
+       // print_r($countries);
 
-        foreach ( $countries as $country ) {
-            $this->out($country->name->common);
+        
+        foreach ($countries as $country) {
+            //$this->out($country->name->common);
+            
+        	$countryTable = TableRegistry::get('Countries');
+        	$countryName = $countryTable -> newEntity();
+        	$countryOfficial = $countryTable ->newEntity();
+        	$countryName -> name = $country -> name -> common;
+        	$countryOfficial -> official = $country -> name -> official;
+        	//$save = $countryName;
+        	//echo $countryName. " " . $countryOfficial;
+        	//$countryTable -> save($save);
+        	//echo $save;
+        	$countryTable -> save($countryName);
+
+        	
         }
 
     }
